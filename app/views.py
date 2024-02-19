ad999a2e8bf032b1f59175e51a7f853b58877c47
Jariel-JonMiller <miller.jarieljon@gmail.com>
2024-02-18 19:58:23 -0500

    initial commit of everything for lab 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import datetime
 
 ###
 # Routing for your application.
@@ -11,12 +11,16 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile(date = '2020, 3, 7'):
+    """Render website's profile page."""     
+    date_joined = format_date_joined()
+    return render_template('profile.html', date_joined=date_joined)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -45,3 +49,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def format_date_joined():
+    now = datetime.now() # today's date 
+    return("Joined " + str(now.strftime("%B")) + ', ' + str(now.strftime("%Y")))
+
+'''date_joined = format_date_joined()'''
